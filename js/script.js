bf5dba86850f3740d43c8596c994f1fd103367cf
Daria <101666130+DariaChernomorets@users.noreply.github.com>
2022-07-02 13:30:05 +0300

    done

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let name = prompt("What is your name?")
+
+if (name == null || name ==="") {
+    alert("User didn't enter the name")
+}
+else {
+    alert("Hello, " + name + "! " + "How are you?")
+}
