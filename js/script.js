3146cc061abff2b89c1354d85507a3a7032dd7e8
Daria <101666130+DariaChernomorets@users.noreply.github.com>
2022-07-03 07:59:43 +0300

    Update script.js
    var upd to const

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const name = prompt("What is your name?");
 if (name == null || name ==="") {
     alert("User didn't enter the name. Please try again!");
 }
+
 else {
     alert("Hello, " + name + "! " + "How are you?")
 }
